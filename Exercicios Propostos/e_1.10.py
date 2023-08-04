40ee5119c40d9d723144fc3c286d091e076d9b4c
MuriloRohor <murilo.marquezini@gmail.com>
2023-08-04 20:04:29 -0300

    Exercicio 1.10

---
+++ Exercicios Propostos/e_1.10.py
@@ -0,0 +1,5 @@
+# Exercício 1.10 Peça ao usuário para digitar dois números e calcule a potência do primeiro número pelo segundo.
+a = int(input("Digite um número: "))
+b = int(input("Digite outro número: "))
+
+print(f"O número {a} elevado à {b} = {a ** b}")
