2470cf3af488efc6415b322260232178ecc79e2c
MuriloRohor <murilo.marquezini@gmail.com>
2023-08-04 12:16:18 -0300

    Criando Repositorio

---
+++ Exercicios Propostos/e_1.1.py
@@ -0,0 +1,6 @@
+# Exercício 1.1 Armazene seu nome e idade em variáveis separadas e imprima uma saída formatada com elas. 
+
+nome = input("Digite seu Nome: ");
+idade = input("Digite sua Idade: ");
+
+print(f"Nome: {nome}\nIdade: {idade}")
---
+++ Exercicios Propostos/e_1.2.py
@@ -0,0 +1,10 @@
+# Exercício 1.2 Armazene dois números em variáveis e imprima a soma, subtração, multiplicação e divisão deles.
+
+n1 = int(input("Digite um numéro: "))
+n2 = int(input("Digite outro numéro: "))
+
+print(f"{n1} + {n2} = {n1 + n2}")
+print(f"{n1} - {n2} = {n1 - n2}")
+print(f"{n1} * {n2} = {n1 * n2}")
+print(f"{n1} / {n2} = {n1 / n2}")
+
