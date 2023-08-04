058cb380b623d42a572df381763dbde2383dc48a
MuriloRohor <murilo.marquezini@gmail.com>
2023-08-04 19:52:28 -0300

    Exercicio 1.8

---
+++ Exercicios Propostos/e_1.8.py
@@ -0,0 +1,5 @@
+# Exercício 1.8 Peça ao usuário para digitar um número e calcule a raiz quadrada desse número. 
+import math
+n = int(input("Digite um número: "))
+
+print(f"Raiz quadrada de {math.sqrt(n):.2f}")
