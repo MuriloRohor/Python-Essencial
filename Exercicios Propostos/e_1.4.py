d30c542b94c2d204eaaa23dc3d6a27fac8d98f33
MuriloRohor <murilo.marquezini@gmail.com>
2023-08-04 15:08:56 -0300

    Exercicio 1.4 e 1.5

---
+++ Exercicios Propostos/e_1.4.py
@@ -0,0 +1,7 @@
+# Exercício 1.4 Peça ao usuário para digitar seu peso e altura, calcule o índice de massa corporal (IMC) e imprima o resultado.
+
+p = int(input("Digite seu peso: "))
+a = float(input("Digite sua altura: "))
+
+imc = p / (a ** 2)
+print(f"Seu IMC : {imc}")
---
+++ Exercicios Propostos/e_1.5.py
@@ -0,0 +1,6 @@
+# 1.5 Peça ao usuário para digitar três números e imprima a soma deles
+a = int(input("Digite um número: "))
+b = int(input("Digite um número: "))
+c = int(input("Digite um número: "))
+
+print(f"{a} + {b} + {c} = {a + b + c}")
