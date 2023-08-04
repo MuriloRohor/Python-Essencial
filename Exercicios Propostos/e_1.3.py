98baad82d398ab7ba30039171ecffea52071d50e
MuriloRohor <murilo.marquezini@gmail.com>
2023-08-04 14:12:30 -0300

    Exercicio 1.3

---
+++ Exercicios Propostos/e_1.3.py
@@ -0,0 +1,7 @@
+# Exercício 1.3 Peça ao usuário para digitar três números, armazene-os em variáveis e imprima a
+# média aritmética dos três números.
+a = int(input("Entre com um número: "))
+b = int(input("Entre com um número: "))
+c = int(input("Entre com um número: "))
+
+print(f"Média Aritmética : {(a + b + c) / 3}")
