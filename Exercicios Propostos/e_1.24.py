d8547650838a13b8c5541a0ed9df19801ab35df0
MuriloRohor <murilo.marquezini@gmail.com>
2023-08-09 11:26:23 -0300

    Exercicio 1.24

---
+++ Exercicios Propostos/e_1.24.py
@@ -0,0 +1,7 @@
+# Exercício 1.24 Calcule o perímetro de um círculo dado o seu raio como entrada. 
+import math
+
+r = float(input("Raio Circulo: "))
+p = 2 * math.pi * r
+
+print(f"Perímetro Círculo: {p:.2f}")
