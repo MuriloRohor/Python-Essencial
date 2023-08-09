91a359e694f191edd5f87e4a401a755b0fe83522
MuriloRohor <murilo.marquezini@gmail.com>
2023-08-09 09:29:49 -0300

    Exercicio 1.17

---
+++ Exercicios Propostos/e_1.17.py
@@ -0,0 +1,16 @@
+# Exercício 1.17 Peça ao usuário para digitar o preço de uma mercadoria, o desconto e o imposto e calcule o preço final da mercadoria. 
+
+m = float(input("Preço Produto: "))
+d = float(input("Desconto Produto(%): "))
+i = float(input("Imposto Produto(%): "))
+
+
+
+
+f = m - (m * (d/100))
+f += (f * (i/100))
+
+print(f"Preço Produto: {m:.2f}\nImposto: {i} %\nDesconto: {d} %\nPreço Final: {f:.2f}")
+
+
+
