9d171d13337d098ceb0d740070c9c3b73cfe5acd
MuriloRohor <murilo.marquezini@gmail.com>
2023-08-09 11:38:45 -0300

    Exercicio 1.26

---
+++ Exercicios Propostos/e_1.26.py
@@ -0,0 +1,8 @@
+# Exercício 1.26 Calcule a área de um triângulo retângulo dadas as medidas dos seus catetos como entrada.
+
+c1 = float(input("Comprimento Cateto: "))
+c2 = float(input("Comprimento Cateto: "))
+
+a = 0.5 * c1 * c2
+
+print(f"Área: {a:.2f} M²")
