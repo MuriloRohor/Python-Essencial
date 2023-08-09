212c25385b3cdabaa2052d4d3a64fe61c0c1111b
MuriloRohor <murilo.marquezini@gmail.com>
2023-08-09 11:54:13 -0300

    Exercicio 1.29

---
+++ Exercicios Propostos/e_1.29.py
@@ -0,0 +1,9 @@
+# Exercício 1.29 Crie um dicionário com os nomes, idades e cor dos olhos de 5 (cinco) membros dasua família.
+
+dic = {
+        'membro1' : {'nome': 'fulano', 'idade': 30, 'olhos': 'preto',},
+        'membro2' : {'nome': 'ciclano', 'idade': 35, 'olhos': 'preto',},
+        'membro3' : {'nome': 'beltrano', 'idade': 40, 'olhos': 'preto',},
+        'membro4' : {'nome': 'fulano', 'idade': 45, 'olhos': 'preto',},
+        'membro5' : {'nome': 'ciclano', 'idade': 50, 'olhos': 'preto',}
+    }
