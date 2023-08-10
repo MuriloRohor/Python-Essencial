e25457c4676451c494c8e22199f535278bd8beab
MuriloRohor <murilo.marquezini@gmail.com>
2023-08-10 20:36:06 -0300

    Exercicio 2.6

---
+++ Estruturas Condicionasi - Exercicios/e_2.6.py
@@ -0,0 +1,21 @@
+# Exercício 2.6 Escreva um programa que verifique se uma string é um palíndromo ou não (não usarestrutura de repetição). 
+
+def eh_palindromo(frase):
+    frase = frase.replace(" ", "").lower()
+    return frase == frase[::-1]
+
+
+frase = input("Digite uma palavra ou frase: ")
+
+if eh_palindromo(frase):
+    print("É um palíndromo!")
+else:
+    print("Não é um palíndromo!")
+
+
+
+
+
+
+
+
