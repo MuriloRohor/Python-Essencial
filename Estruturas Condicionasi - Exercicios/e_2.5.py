072132b2fd490e66c6c8d2263a66cc5605ea2edb
MuriloRohor <murilo.marquezini@gmail.com>
2023-08-10 19:44:34 -0300

    Exercicio 2.5

---
+++ Estruturas Condicionasi - Exercicios/e_2.5.py
@@ -0,0 +1,8 @@
+# Exercício 2.5 Escreva um programa que, a partir da idade digitada pelo usuário, verifique se ele é maior de idade ou não .
+
+i = int(input("Digite a idade"))
+
+if i > 17:
+    print("Você é maior de idade")
+else:
+    print("Você não é maior de idade")
