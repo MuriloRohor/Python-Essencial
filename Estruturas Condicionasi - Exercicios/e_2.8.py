d558fbd99f792771183973ecf881276d70291b26
MuriloRohor <murilo.marquezini@gmail.com>
2023-08-10 21:29:27 -0300

    Exercicio 2.8

--- Estruturas Condicionasi - Exercicios/e_2.8.py
+++ Estruturas Condicionasi - Exercicios/e_2.8.py
@@ -1,9 +1,20 @@
 # Exercício 2.8 Escreva um programa que verifique se uma string é um número real ou não (pode usar estrutura de repetição).
-str_ = input("Numero: ")
 
-if str_.isnumeric():
-     n = int(str_)
-     if n > -1:
-          print("é um numero real")
+s = input("Digite numero inteiro: ")
+eh_real = True
+ponto_encontrado = False
+
+for char in s:
+     if char == '.':
+         if ponto_encontrado:
+             eh_real = False
+             break
+         ponto_encontrado = True
+     elif not char.isdigit():
+         eh_real = False
+         break
+       
+if eh_real:
+    print("A string é um número real")
 else:
-     print("Não é um numero inteiro")
+    print("A string não é um número real")
