a1284348971a4260d0ba2388500b05b175f95853
MuriloRohor <murilo.marquezini@gmail.com>
2023-08-21 19:21:52 -0300

    Exercicio 2.11

---
+++ 02 - Estruturas Condicionasi - Exercicios/e_2.11.py
@@ -0,0 +1,11 @@
+# Exercício 2.11 Escreva um programa que verifique se uma temperatura está acima, abaixo ou 
+# dentro da faixa normal (36°C a 37°C). 
+
+temp = float(input("Digite sua temperatura: "))
+
+if temp > 37:
+    print("Temperatura acima do normal!")
+elif temp < 36:
+    print("Temperatura abaixo do normal!")
+else:
+    print("Temperatura normal!")
