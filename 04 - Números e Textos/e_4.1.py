79c39c44c6b257eb613a22fdac2899ef2cc4a4ca
MuriloRohor <murilo.marquezini@gmail.com>
2023-08-30 20:59:28 -0300

    Exercicio 4.1

---
+++ 04 - Números e Textos/e_4.1.py
@@ -0,0 +1,7 @@
+# Exercício 4.1 Crie um programa que peça ao usuário para inserir dois números e calcule a potência
+# do primeiro número pelo segundo número. 
+
+a = int(input("Digite um número: "))
+b = int(input("Digite outro número: "))
+
+print(f'{a} ** {b} = {a**b}')
