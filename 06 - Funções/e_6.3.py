e71855e8e75181e82ff2ac0927aab5ca800dcfff
MuriloRohor <murilo.marquezini@gmail.com>
2023-08-31 19:38:40 -0300

    Exercicio 6.3

---
+++ 06 - Funções/e_6.3.py
@@ -0,0 +1,5 @@
+# Exercício 6.3 Crie uma função que receba três argumentos posicionais: um número inteiro, um
+# número flutuante e uma string. A função deve imprimir os valores dos argumentos na tela.
+
+def imprimirNumeros(a: int, b: float, c: str):
+    print(a, b, c)
