d24b1970aee5712ff9ee92c01399399847976952
MuriloRohor <murilo.marquezini@gmail.com>
2023-08-30 21:08:13 -0300

    Exercico 4.3

---
+++ 04 - Números e Textos/e_4.3.py
@@ -0,0 +1,11 @@
+# Exercício 4.3 Crie um programa que peça ao usuário para inserir um ângulo em graus e calcule o seno, cosseno e tangente desse ângulo.
+import math
+
+a = int(input("Digite um angulo º: "))
+
+print(f'Seno : {math.sin(a):.2f}')
+print(f'cosseno  : {math.cos(a):.2f}')
+print(f'tangente : {math.tan(a):.2f}')
+
+ 
+
