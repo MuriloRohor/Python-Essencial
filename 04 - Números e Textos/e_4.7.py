49d9d6aa6825a7301aa2c9d22cdc6e985a2adfa5
MuriloRohor <murilo.marquezini@gmail.com>
2023-08-31 19:31:17 -0300

    Exercicio 6.1

---
+++ 04 - Números e Textos/e_4.7.py
@@ -0,0 +1,4 @@
+# Exercício 4.7 Crie um programa que peça ao usuário para digitar um nome de usuário e uma senha
+# contendo apenas caracteres alfanuméricos e use expressão regular para fazer uma limpeza nos
+# valores digitados, exibindo-os novamente para o usuário os valores que forem modificados
+
---
+++ 06 - Funções/e_6.1.py
@@ -0,0 +1,6 @@
+# Exercício 6.1 Crie uma função que receba dois números como argumentos e retorne a soma dos dois números. 
+
+def soma(a, b):
+    return a + b
+
+print(soma(1, 2))
