172a2e69b75ee56cefdb89c93ef17f5b29617d9b
MuriloRohor <murilo.marquezini@gmail.com>
2023-08-30 21:17:10 -0300

    Exercicio 4.5

---
+++ 04 - Números e Textos/e_4.5.py
@@ -0,0 +1,7 @@
+# Exercício 4.6 Crie um programa que peça ao usuário para inserir dois números complexos e calcule
+# a soma e o produto desses números.
+
+n = float(input("Digite um número: "))
+n2 = float(input("Digite um número: "))
+
+print(f"Soma : {n+n2:.2f} | Produto : {n*n2:.2f}")
