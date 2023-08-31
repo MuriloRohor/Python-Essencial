bab9d171749ff9f57315b659648a22da05693c13
MuriloRohor <murilo.marquezini@gmail.com>
2023-08-30 21:03:11 -0300

    Exercicio 4.2

---
+++ 04 - Números e Textos/e_4.2.py
@@ -0,0 +1,6 @@
+# Exercício 4.2 Crie um programa que peça ao usuário para inserir um número e calcule a raiz quadrada desse número.
+import math
+
+n = int(input("Digite um número: "))
+
+print(f"A raiz quadrada de {n} = {math.sqrt(n):.2f}")
