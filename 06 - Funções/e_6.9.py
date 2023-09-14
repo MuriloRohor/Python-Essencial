906c5f30bb3ce168f3331a0795a0eb70c3f6e498
MuriloRohor <murilo.marquezini@gmail.com>
2023-09-14 15:27:34 -0300

    Exercicio 6.9

--- 06 - Funções/e_6.9.py
+++ 06 - Funções/e_6.9.py
@@ -1,1 +1,12 @@
 # Exercício 6.9 Crie uma função recursiva que calcule o fatorial de um número inteiro.
+
+def fatorial(n):
+    """Calcula o fatorial de um número inteiro n usando recursão."""
+    if n == 0:
+        return 1
+    else:
+        return n * fatorial(n-1)
+
+# Testando a função
+num = int(input("Digite um número inteiro: "))
+print(f"Fatorial de {num} é {fatorial(num)}")
